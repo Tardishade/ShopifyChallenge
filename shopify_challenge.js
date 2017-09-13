87e74f703ff1815e15bb103b3253bfa843e82f52
Anirudh Iyer <lopaneyo@gmail.com>
2017-09-13 13:29:38 -0400

    commit

--- shopify_challenge.js
+++ shopify_challenge.js
@@ -1,20 +1,35 @@
 var http = require('http');
-
-var data;
-
-http.get('http://backend-challenge-winter-2017.herokuapp.com/customers.json',(res) => {
-    res.setEncoding('utf8');
-          res.on('data', (body) => {console.log(body);
-            data = body;});
-        });
-
-var json = JSON.parse(data);
-var validations = json.validations;
-var customers = json.customers;
-var attributes = ["required", "type", "length"];
 var invalid_customers = [];
 
-function validateCustomer(customer) {
+
+http.get('http://backend-challenge-winter-2017.herokuapp.com/customers.json', (res) => {
+    res.setEncoding('utf8');
+    res.on('data', (body) => {
+        splitJson(body);
+    });
+});
+
+
+function splitJson(body) {
+    var json = JSON.parse(body);
+    var validations = json.validations;
+    var customers = json.customers;
+
+    customers.forEach((customer) => {
+        validateCustomer(customer, validations);
+    });
+
+    var final_object = {
+        "invalid_customers": invalid_customers
+    };
+
+    console.log(final_object);
+}
+
+
+
+
+function validateCustomer(customer, validations) {
     var invalid_fields = [];
     validations.forEach((validation) => {
         var thing = Object.keys(validation)[0];
@@ -25,36 +40,33 @@ function validateCustomer(customer) {
                 else {
                     invalid_fields.push(thing);
                 }
-            } 
+            }
         }
         if ((validation[thing])["type"] !== undefined) {
-            if ((validation[thing])["type"] !== typeof(value)) {
+            if ((validation[thing])["type"] !== typeof (value)) {
                 invalid_fields.push(thing);
             }
         }
         if ((validation[thing])["length"] !== undefined) {
-            if(((validation[thing])["length"])["min"] !== undefined) {
-                if(((validation[thing])["length"])["min"] >= value.length){}
-                else {
+            if (((validation[thing])["length"])["min"] !== undefined) {
+                if (((validation[thing])["length"])["min"] >= value.length) {} else {
                     invalid_fields.push(thing);
                 }
             }
-            if(((validation[thing])["length"])["max"] !== undefined) {
-                if(((validation[thing])["length"])["max"] <= value.length){}
-                else {
+            if (((validation[thing])["length"])["max"] !== undefined) {
+                if (((validation[thing])["length"])["max"] <= value.length) {} else {
                     invalid_fields.push(thing);
                 }
             }
-        } 
-    });       
-    
+        }
+    });
+
+    var customerObj = {
+        "id": customer["id"],
+        "invalid_fields": JSON.stringify(invalid_fields)
+    };
+
+    if (invalid_fields.length > 0) {
+        invalid_customers.push(customerObj);
+    }
 }
-
-customers.forEach((customer) => {
-    validateCustomer(customer);    
-});
-
-var final_object = {"invalid_customers": invalid_customers};
-
-console.log(final_object);
-
